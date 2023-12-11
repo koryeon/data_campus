74e51f9f7f8bd0bd22cd903e313ca65e1dc018e2
koryeon <gydus241@gmail.com>
2023-12-11 16:08:43 +0900

    commit maessage

---
+++ fintech/fin_tech_230801/example03.js
@@ -0,0 +1,25 @@
+function plus (p1,p2){
+    console.log(p1+p2);
+}
+
+const plusArrowFunc = (p1,p2) =>{
+    console.log(p1+p2);
+};
+
+const minusArrowFunc = (p1,p2) =>{
+    console.log(p1-p2);
+};
+
+const multiArrowFunc = (p1,p2) =>{
+    console.log(p1*p2);
+};
+
+const divArrowFunc = (p1,p2) =>{
+    console.log(p1/p2);
+};
+
+plus(5,10);
+plusArrowFunc(5,10);
+minusArrowFunc(5,10);
+multiArrowFunc(5,10);
+divArrowFunc(5,10);
